acc03248ef4935a71ed29926bd2742d22975f94f
MaruthiCharan2403 <yssmc24#gmail.com>
2025-07-22 22:16:21 +0530

    done

--- flaskbackend/gemini_utils.py
+++ flaskbackend/gemini_utils.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCxc3QLpDeV11PkXp0fmfZCARBuTz3oYmA")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def call_gemini_api(prompt):
     response = genai.GenerativeModel("gemini-1.5-flash").generate_content(prompt)
